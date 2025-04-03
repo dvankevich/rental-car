22f1debb5ada810c1a66d0801c442cffc5456cb5
dvankevich <dvankevich@gmail.com>
2025-04-03 10:43:13 +0300

    rewrite Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,22 @@
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
+import clsx from 'clsx';
 import s from './Header.module.css';
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(s.navLink, isActive && s.active);
+};
 
 const Header = () => {
   return (
     <>
       <header className={s.header}>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/catalog">Catalog</Link>
-            </li>
-          </ul>
+        <nav className={s.navigation}>
+          <NavLink to="/" className={buildLinkClass}>
+            Home
+          </NavLink>
+          <NavLink to="/catalog" className={buildLinkClass}>
+            Catalog
+          </NavLink>
         </nav>
       </header>
       <Outlet />
